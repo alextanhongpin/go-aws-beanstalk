8f3d5de56a4fa9d3d06c96137dfe9170b826c450
Alex Tan Hong Pin <alextan@seekasia.com>
2017-07-07 23:15:51 +0800

    Update application.go

--- application.go
+++ application.go
@@ -4,17 +4,25 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
 	fmt.Printf("Hello world, got port *:%s", port)
-
-	http.HandleFunc("/cron", func(w http.ResponseWriter, r *http.Request) {
+	
+	// We are using a third-party library here to demonstrate an example of using package in aws beanstalk
+	r := httprouter.New()
+	r.POST("/cron", func(w http.ResponseWriter, r *http.Request) {
+		// Carry out the cron job
+		log.Println("Running cron")
 		fmt.Fprint(w, "Hello world")
 	})
-	fmt.Printf("running cron job at port *:%s", port)
-	http.ListenAndServe(":"+port, nil)
-
+	
+	log.Printf("Listening to server at port *:%s\n", port)
+	http.ListenAndServe(":"+port, r)
 }
