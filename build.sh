a803044ee5514872da16260610865a116d647875
Alex Tan Hong Pin <alextan@seekasia.com>
2017-07-07 23:18:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-go get ./...
+# This will not work unfortunately
+# go get ./...
+
+# You have to manually declare all your dependencies here
+go get github.com/julienschmidt/httprouter
+
+# Build your application to the bin directory
 go build -o bin/application application.go
