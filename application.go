819f138c674f17485c7cf437ce66e2f6f15339e2
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-07-07 14:50:11 +0800

    Update application

--- application.go
+++ application.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 
 	port := os.Getenv("PORT")
-	// fmt.Printf("Hello world, got port *:%s", port)
+	fmt.Printf("Hello world, got port *:%s", port)
 
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprint(w, "Hello world")
-	// })
-	// http.ListenAndServe(":"+port, nil)
-
+	http.HandleFunc("/cron", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello world")
+	})
 	fmt.Printf("running cron job at port *:%s", port)
+	http.ListenAndServe(":"+port, nil)
+
 }
