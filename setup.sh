ad2806611819be75858aeb23932ea956f3b78b2b
Alex Tan Hong Pin <alextan@seekasia.com>
2017-07-07 23:46:22 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
+# Another option for deploying the application is by creating a binary
+# Since AWS is running linux, we need to create a linux executable for go
 GOARCH=amd64 GOOS=linux go build -o bin/application application.go
